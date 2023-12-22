444e5d7039cf0eed3168bc1f977f96822dd7471c
Hitsam Tiammar <lossantos3000@gmail.com>
2023-12-22 23:15:02 +0700

    implement eslint, refactor checkDiagonal and modify readme.md

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import TicTacToe from './component/TicTacToe';
@@ -9,29 +9,34 @@ function App() {
   const [numCol, setNumCol] = useState(3);
   const [page, setPage] = useState('landing');
 
-  function onToGame(size){
-    setNumCol(size)
-    setPage('tictactoe')
+  function onToGame(size) {
+    setNumCol(size);
+    setPage('tictactoe');
   }
 
-  function renderPage(){
-    if(page === 'landing'){
-      return <Landing onToGame={onToGame} />
-    }else if(page === 'tictactoe'){
-      return <TicTacToe onExit={() => setPage('landing')} numCol={numCol} />
+  function renderPage() {
+    if (page === 'landing') {
+      return <Landing onToGame={onToGame} />;
+    } else if (page === 'tictactoe') {
+      return <TicTacToe onExit={() => setPage('landing')} numCol={numCol} />;
     }
     return null;
   }
 
   return (
     <div className="App">
-      <Navbar expand="lg" bg="primary" className="position-sticky top-0" data-bs-theme="dark" >
+      <Navbar
+        expand="lg"
+        bg="primary"
+        className="position-sticky top-0"
+        data-bs-theme="dark"
+      >
         <Container>
           <Navbar.Brand>Tic Tac Toe</Navbar.Brand>
         </Container>
       </Navbar>
       <Container className="d-flex justify-content-center flex-1">
-          {renderPage()}
+        {renderPage()}
       </Container>
     </div>
   );
--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,28 +1,35 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
 
 export default function Landing({ onToGame }) {
   const [size, setSize] = useState('3');
-  function onSubmit(){
-    onToGame(Number(size))
+  function onSubmit() {
+    onToGame(Number(size));
   }
 
   return (
-    <div className="d-flex align-items-center flex-column w-50 py-5" id="landing">
-        <h2 className="fw-bold">Select Size</h2>
-        <Form.Select onChange={(e) => setSize(e.target.value)} value={size} className="my-3">
-          <option value="3">3</option>
-          <option value="6">6</option>
-          <option value="9">9</option>
-          <option value="12">12</option>
-        </Form.Select>
-        <Button onClick={onSubmit} >Go To Game</Button>
+    <div
+      className="d-flex align-items-center flex-column w-50 py-5"
+      id="landing"
+    >
+      <h2 className="fw-bold">Select Size</h2>
+      <Form.Select
+        onChange={(e) => setSize(e.target.value)}
+        value={size}
+        className="my-3"
+      >
+        <option value="3">3</option>
+        <option value="6">6</option>
+        <option value="9">9</option>
+        <option value="12">12</option>
+      </Form.Select>
+      <Button onClick={onSubmit}>Go To Game</Button>
     </div>
-  )
+  );
 }
 
 Landing.propTypes = {
-  onToGame: PropTypes.func.isRequired
-}
+  onToGame: PropTypes.func.isRequired,
+};
